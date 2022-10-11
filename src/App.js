4c9947b159eaffe48be6d9df1bd7f74eb98910ed
akshatk806 <akshatk806@gmail.com>
2022-10-11 19:29:42 +0530

    2. Adding the components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 
+// rcc -> snipet
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <Navbar />
+        <News />
       </div>
     )
   }
