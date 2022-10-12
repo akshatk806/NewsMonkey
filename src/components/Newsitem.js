cb8ab55fc8dadb38376f6d594f11891e781ab433
akshatk806 <akshatk806@gmail.com>
2022-10-12 20:59:53 +0530

    5. Populating the news

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
-        let { title, description, imageURL} = this.props;       // destructuring
+        let { title, description, imageURL, newsURL} = this.props;       // destructuring
         return (
             <div className="my-3">
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageURL} className="card-img-top" alt="..." />
+                    <img src={imageURL} className="card-img-top" alt={title} />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/" className="btn btn-primary btn-sm">Read More</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}.....</p>
+                        <a href={newsURL} target="_blank" rel="noreferrer noopener" className="btn btn-primary btn-sm">Read More</a>
                     </div>
                 </div>
             </div>
