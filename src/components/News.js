3a18f3009bd8aa4b1646bfcca9089ba8c58a1942
akshatk806 <akshatk806@gmail.com>
2022-10-12 20:37:33 +0530

    4. Setting the state

--- src/components/News.js
+++ src/components/News.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class News extends Component {
+    constructor(){
+        super();
+        console.log("Hello I am a constructor from news component");
+
+        // Setting the state
+        this.state={          
+
+        }
+    }
+
     render() {
         return (
-            <div className='container my-3'>
+            <div className='container'>
                 <h1>NewsMonkey - Top Headlines</h1>
                 <div className="row">
                     <div className="col-md-4">
-                        <Newsitem title="myTitle" description="myDescription" />
+                        <Newsitem title="myTitle" description="myDescription" imageURL="https://images.indianexpress.com/2022/10/saurav-ganguly-1200-1.jpg" newsURL="https://www.ndtv.com/india-news/can-you-identify-chief-minister-president-asks-tea-workers-heres-why-3424868"/>
                     </div>
                     <div className="col-md-4">
                         <Newsitem />
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
-        let {title,description}=this.props;       // destructuring
+        let { title, description, imageURL} = this.props;       // destructuring
         return (
-            <div className="card" style={{width: "18rem"}}>
-                <img src="..." className="card-img-top" alt="..." />
+            <div className="my-3">
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={imageURL} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/" className="btn btn-primary">Go somewhere</a>
+                        <a href="/" className="btn btn-primary btn-sm">Read More</a>
                     </div>
+                </div>
             </div>
         )
     }
