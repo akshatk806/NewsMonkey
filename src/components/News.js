1e0cd084ecb0c621fab8e0c9e5a7ffcd49b428b8
akshatk806 <akshatk806@gmail.com>
2022-10-14 23:30:24 +0530

    11. Adding author name and time of publishing

--- src/components/News.js
+++ src/components/News.js
@@ -88,7 +88,7 @@ export class News extends Component {
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div key={element.url} className="col-md-4">
-                            <Newsitem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageURL={element.urlToImage} newsURL={element.url} />
+                            <Newsitem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageURL={element.urlToImage} newsURL={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                         </div>
                     })}
                 </div>
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
     render() {
-        let { title, description, imageURL, newsURL} = this.props;       // destructuring
+        let { title, description, imageURL, newsURL, author, date, source } = this.props;       // destructuring
         return (
             <div className="my-3">
                 <div className="card">
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:"90%",zIndex:'1'}}>
+                        {source}
+                    </span>
                     <img src={imageURL} className="card-img-top" alt={title} />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}.....</p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toUTCString()}</small></p>
                         <a href={newsURL} target="_blank" rel="noreferrer noopener" className="btn btn-dark btn-sm">Read More</a>
                     </div>
                 </div>
